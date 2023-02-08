6d4d86cc4940fdff240cf36c41cd463872a68f19
Adam Ulrich <adamulrich@hotmail.com>
2023-02-07 21:16:32 -0800

    connecting to our db

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'cse341-instructor.herokuapp.com',
-  schemes: ['https']
+  host: '',
+  schemes: []
 };
 
 const outputFile = './swagger.json';
